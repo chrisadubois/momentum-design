235e54afea43302174b840b30357b198d87361f7
Christopher DuBois <chdubois@cisco.com>
2022-10-26 15:08:37 -0700

    feat(pkg): add package scaffolding base

--- config/plop/actions/AddWorkspacePackage.ts
+++ config/plop/actions/AddWorkspacePackage.ts
@@ -13,5 +13,5 @@ export const AddWorkspacePackage: ActionType = {
   type: 'addMany',
   base: `${join(`${folders.templates}`, `${ACTION.ADD}`, `${TYPE.WORKSPACE}`)}`,
   templateFiles: `${join(`${folders.templates}`, '**', `*.${FILE_TYPE.DEFAULT}`)}`,
-  destination: `${join(`${folders.workspace}`, `@momentum-design/${packageName}`)}`,
+  destination: `${join(`${folders.workspace}`, `@momentum-design/{{${packageName}}}`)}`,
 };
